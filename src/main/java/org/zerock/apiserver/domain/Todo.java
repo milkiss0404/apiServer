5320bc587472749e057eaac3ac8dd1bbf288f220
milkiss0404 <sksksk96@naver.com>
2024-10-10 17:56:26 +0900

    sad

---
+++ src/main/java/org/zerock/apiserver/domain/Todo.java
@@ -0,0 +1,26 @@
+package org.zerock.apiserver.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    @Column(length = 500,nullable = false)
+    private String title;
+    private String content;
+    private boolean complete;
+    private LocalDateTime dueDate;
+}
---
+++ src/test/java/org/zerock/apiserver/domain/TodoTest.java
@@ -0,0 +1,85 @@
+package org.zerock.apiserver.domain;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.apiserver.repository.TodoRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+
+@SpringBootTest
+@Log4j2
+class TodoTest {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    public void test1(){
+        Assertions.assertNotNull(todoRepository);
+        log.info(todoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsert() {
+       Todo todo =  Todo.builder()
+                .title("title")
+                .content("content")
+                .dueDate(LocalDate.of(2023,12,30).atStartOfDay())
+                .build();
+        todoRepository.save(todo);
+    }
+    @Test
+    public void testRead() {
+
+        Long tno = 1L;
+        Optional<Todo> byId = todoRepository.findById(tno);
+        Todo todo = byId.orElseThrow();
+        log.info(todo);
+    }
+
+    @Test
+    public void testUpdate(){
+        //given
+        Long tno = 1L;
+
+        Todo existingTodo = todoRepository.findById(tno)
+                .orElseThrow(() -> new IllegalArgumentException("Todo not found with tno: " + tno));
+
+
+        Todo update = Todo.builder()
+                .tno(existingTodo.getTno())
+                .title("타이틀")
+                .content("dasdasdas")
+                .build();
+
+        //when
+        Todo updatedTodo = todoRepository.save(update);
+
+        //then
+        log.info("Updated Todo: {}", updatedTodo);
+    }
+
+    @Test
+    public void testPaging() {
+        for (int i = 0; i < 100; i++) {
+
+
+        }
+        //페이지번호 0부터
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+        Page<Todo> result = todoRepository.findAll(pageable);
+
+        log.info(result.getTotalElements());
+        log.info(result.getContent());
+
+    }
+}
